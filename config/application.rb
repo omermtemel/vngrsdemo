6019a00307e9ababd618cdf483e08f4e145cc8c4
Omer M Temel <omermtemel@gmail.com>
2013-07-30 22:22:37 +0300

    Devise settings

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Vngrsdemo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Devise settings
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,9 +12,11 @@ Vngrsdemo::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -34,4 +36,7 @@ Vngrsdemo::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Devise manuel settings
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
